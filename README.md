647f56c3e27cb3799047ebc9a0b230c9ac3b9af8
Ari Snyder <arisnyder27@gmail.com>
2025-02-12 02:05:06 -0500

    music now runs in stillalive.java and is synced with display

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This repo is a WIP recreation of the Still Alive scene from the ending credits of Portal.
 Run "stillalive.java" in a Terminal / Command line that supports ANSI.
+
 This is a work in progress, and the melody and full lyrics are not done.
+
 Eventually I might get around to converting it to C++
+
 Also, ensure the text all fits in your terminal. if its too small, the ANSI code to go back doesn't seem to work when it reaches the top. if, after it is run, both the top and bottom lines are mostly repeated dashes, its the right size
+Personally, on my windows laptop, full screen terminal on 9 pt font seems to work fine, but play around for the right size!
--- stillalive.java
+++ stillalive.java
@@ -7,21 +7,27 @@ public class stillalive {
         System.out.flush();     
         ui UI = new ui();
         long endtime = 0;
+        Thread newThread = new Thread(() -> playsong.main(args));                                                                                                                                                       
+        newThread.start(); 
         for(int page = 0;page<UI.lyricslist.length;page++){
             Thread.sleep(UI.lyricslist[page][0].before-endtime);
             String linestr = "|";
             int linenum = 1;
             printui(UI);
+            
             for(int i = 0;i<UI.background.length-2;i++){
                 System.out.print("\033[F\r");
                 System.out.print("\033[F\r");
             }
+             
             Lyric[] lyrics = UI.lyricslist[page];
             for(int line = 0;line<lyrics.length;line++){
                 Lyric lyric = lyrics[line];
                 Thread.sleep(lyric.before-endtime);
                 for(int position=0; position<lyric.lyrics.length();position++){
                     linestr+= lyric.lyrics.charAt(position);
+                    
+                    System.out.print("\033[33m"); 
                     System.out.print("\r"+UI.background[linenum]+"\r"+linestr);
                     if(lyric.lyrics.charAt(position)=='\n'){
                         System.out.println();
@@ -30,6 +36,7 @@ public class stillalive {
                         System.out.print("\r"+UI.background[linenum]+"\r"+linestr);
 
                     }
+                    System.out.print("\033[0m");   
                     Thread.sleep(lyric.during/lyric.lyrics.length());
                 }
                 endtime=lyric.before+ lyric.during;
@@ -38,10 +45,12 @@ public class stillalive {
                 System.out.print("\033[F\r");
                 System.out.print("\033[F\r");
             }
+            System.out.print("\033[F\r");
         }
         printui(UI);
     }
     public static void printui(ui UI){
+        System.out.print("\033[33m");  
         for(int i = 0;i<UI.background.length-1;i++){
                 System.out.println();
                 System.out.println(UI.background[i]);
@@ -49,6 +58,8 @@ public class stillalive {
             
             System.out.println();
             System.out.print(UI.background[UI.background.length-1]);
+        System.out.print("\033[0m");   
     }
 
 }
+
